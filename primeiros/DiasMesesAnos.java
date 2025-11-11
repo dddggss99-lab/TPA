47010317c5674697f3e764ac07df0ad84e0d306d
Debora Gomes <dddggss99@gmail.com>
2025-11-11 20:25:01 -0300

    Add files via upload

---
+++ primeiros/DiasMesesAnos.java
@@ -0,0 +1,19 @@
+package prjLista1;
+
+public class DiasMesesAnos {public static void main(String args[]) {
+	double mes, ano, dia;
+	
+	
+	dia=30;
+	
+	mes= dia/30;
+	ano= dia/365;
+	
+	System.out.println(mes);
+	System.out.println(ano);
+	
+	
+	
+}
+
+}
